2fad7ddb19e316d6f7cb1519cdb7a32dc8d55aa9
UNBunny <nikitakuharchuk07@gmail.com>
2024-04-14 02:20:02 +0600

    Task016Test solved OOP (VM Factory)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,33 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.actionButton)
+        textView = findViewById(R.id.titleTextView)
+        progressBar = findViewById(R.id.progressBar)
+        viewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(LiveDataWrapper.Base(), Repository.Base())
+        ).get(MainViewModel::class.java)
+        button.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) { uiState ->
+            uiState.apply(button, progressBar, textView)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModelFactory.kt
@@ -0,0 +1,14 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+class MainViewModelFactory(private val liveDataWrapper: LiveDataWrapper, private val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(liveDataWrapper, repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
