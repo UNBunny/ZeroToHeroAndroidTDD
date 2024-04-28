39c7e9ef6a2fcbfe8a7a9ce2815d349b937f8ff6
UNBunny <nikitakuharchuk07@gmail.com>
2024-04-28 16:18:01 +0600

    Task018Test solved OOP (bundle)

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Repository.kt
@@ -0,0 +1,11 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import kotlinx.coroutines.delay
+
+interface Repository {
+    suspend fun load(): SimpleResponse
+    class Base(private val service: SimpleService, private val url: String) : Repository {
+        override suspend fun load(): SimpleResponse =
+            service.fetch(url)
+    }
+}
